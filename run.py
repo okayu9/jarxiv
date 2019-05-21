77a5effda78c81b5a2272a15d70f495c087d0661
Yumeto Inaoka <in@okayu.jp>
2019-05-21 17:35:54 +0900

    get arxiv feed

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-print('hello, jarxiv!!')
+import os
+import time
+import pprint
+import feedparser
+
+
+def main():
+    ARXIV_BASE_URL = os.environ['ARXIV_BASE_URL']
+    ARXIV_SUBJECT = os.environ['ARXIV_SUBJECT']
+
+    while True:
+        ARXIV_URL = ARXIV_BASE_URL + ARXIV_SUBJECT
+        feed = feedparser.parse(ARXIV_URL)
+        pprint.pprint(feed)
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
